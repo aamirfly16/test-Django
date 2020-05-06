9a1095ad50399c038f53d33229f33b58c6ad307e
aamir.fly16 <aamir.fly16@gmail.com>
2020-05-06 06:45:31 +0530

    adding mdco

--- mdco/mdco/settings.py
+++ mdco/mdco/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'irk^h^3#s^j*(d13ycte*ma=11^fo61bf0ehhfi4(=&#*f7#rq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
